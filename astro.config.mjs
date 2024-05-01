0fa15e6bceed42190e4f31d55357cca8c0fa4fe1
Jose María Barambones Ramírez <j.barambones@madhcilab.es>
2024-05-01 13:37:48 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,7 @@ import alpinejs from "@astrojs/alpinejs";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://xerahort.github.io',
   integrations: [tailwind(), alpinejs()],
   output: 'static'
 });
