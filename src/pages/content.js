a74abe7d5087392aa3300a07cf493c0c60172630
Jose María Barambones Ramírez <j.barambones@madhcilab.es>
2024-05-01 14:15:21 +0200

    Update content.js

--- src/pages/content.js
+++ src/pages/content.js
@@ -1,23 +1,86 @@
 // Personal Information --> #hero section
-export const name = "Peter Griffin"
-export const designation = "Safety Inspector"
-export const location = "Quahog, Rhode Island"
+export const name = "Jose Barambones"
+export const designation = "Professor and researcher in Game UX & AI"
+export const location = "Universidad Politécnica de Madrid, Spain"
 export const pronouns = "he/him"
-export const website = "https://petergriffin.com"
+export const website = "https://jbaram.dev"
 
 // About
-export const about = "An adventurous man working as a safety inspector with a quirky sense of humor. Husband to Louis Griffin. Father of Chris, Brian, and Stewie."
+export const about = "I am a Senior Researcher at the Madrid Human-Computer Interaction Laboratory (MadHCILab) and Assistant Professor on Human-Computer Interaction and Software Engineering at the Escuela Técnica Superior de Ingenieros Informáticos of the Universidad Politécnica de Madrid (ETSIINF, UPM). My background includes teaching on Programming, HCI and Game Design subjects at Universidad CEU San Pablo, and the Centro Universitario de Tecnología, Arte y Diseño (U-TAD) of the Universidad Camilo Jose Cela. My research is mainly focused on Artificial Intelligence and User Experience on Games, in particular on Inteligent Virtual Agents, Multi-Agent Systems, and Human-Agent Interaction in real-time scenarios."
 
 // Work Experience --> #work section
 export const work = [
 	{
-		from: "2001",
+		from: "2021",
 		to: "Present",
-		role: "Safety Inspector",
-		org: "Quahog Safety Department",
-		org_url: "https://quahog.gov/safety",
-		location: "Quahog, Rhode Island",
-		skills: "Safety Inspection, Risk Assessment, Emergency Response"
+		role: "Assistant Professor",
+		org: "Escuela Técnica Superior de Ingenieros Informáticos (ETSIInf-UPM)",
+		org_url: "https://www.fi.upm.es",
+		location: "Campus de Montegancedo, Boadilla del Monte, Madrid, Spain",
+		skills: "Interacción Persona-Ordenador, Fundamentos de Videojuegos, Sistemas Interactivos, User Experience and Mobile Interaction, Agent-Based Software Development"
+	},
+	{
+		from: "2019",
+		to: "Present",
+		role: "Researcher",
+		org: "Centro de Tecnologia Biomédica (CTB-UPM)",
+		org_url: "http://www.ctb.upm.es",
+		location: "Campus de Montegancedo, Boadilla del Monte, Madrid, Spain",
+		skills: "UX and Usability for Older Users, Human Cognitive Analysis, and Gamification (AgeingLab)"
+	},
+	{
+		from: "2020",
+		to: "2021",
+		role: "Associate Professor",
+		org: "Centro Universitario de Tecnología y Arte Digital (U-TAD), Universidad Camilo Jose Cela",
+		org_url: "https://u-tad.com",
+		location: "Parque Europa Empresarial, Las Rozas, Madrid, Spain",
+		skills: "Introduction to Programming, Diseño para RV y RA, Programación en C#"
+	},
+	{
+		from: "2020",
+		to: "2021",
+		role: "Associate Professor",
+		org: "Escuela Politécnica Superior, Universidad CEU San Pablo",
+		org_url: "https://www.uspceu.com",
+		location: "Campus de Montepríncipe, Alcorcón, Madrid, Spain",
+		skills: "Programación I, Programación II, Fundamentos de Computadores, Metodologías de Diseño Software"
+	},
+	{
+		from: "2019",
+		to: "2020",
+		role: "Collaborating Professor",
+		org: "Escuela Técnica Superior de Ingenieros Informáticos (ETSIInf-UPM)",
+		org_url: "https://www.fi.upm.es",
+		location: "Campus de Montegancedo, Boadilla del Monte, Madrid, Spain",
+		skills: "Interacción Persona-Ordenador"
+	},
+	{
+		from: "January 2019",
+		to: "July 2019",
+		role: "PhD Research Intern",
+		org: "National Institute of Informatics (NII)",
+		org_url: "https://www.nii.ac.jp/en/",
+		location: "Hitotsubashi, Chiyoda City, Tokyo, Japan",
+		skills: "Distributed Artificial Intelligence, Optimization and Operations Research, Multi-Agent Systems"
+	},
+	{
+		from: "2016",
+		to: "2015",
+		role: "Project Associate Engineer",
+		org: "European Organization for Nuclear Research (CERN)",
+		org_url: "https://home.cern",
+		location: "Espl. des Particules, Meyrin, Switzerland",
+		skills: "Distributed Artificial Intelligence, Optimization and Operations Research"
+	},
+	{
+		from: "January 2015",
+		to: "July 2015",
+		role: "MCS Research Intern",
+		org: "National Institute of Informatics (NII)",
+		org_url: "https://www.nii.ac.jp/en/",
+		location: "Hitotsubashi, Chiyoda City, Tokyo, Japan",
+		skills: "Distributed Artificial Intelligence, Multi-Agent Systems"
 	}
 ]
 
@@ -25,52 +88,60 @@ export const work = [
 export const projects = [
 	{
 		from: "2023",
-		to: "Present",
-		title: "Project Griffin",
-		description: "A project to improve safety standards in Quahog by implementing innovative safety measures.",
-		url: "https://projectgriffin.com",
-		tech: "Safety Equipment, Training Programs, Community Outreach",
+		to: "2026",
+		title: "MOTIVA",
+		description: "Ecosistema computacional con apoyo motivacional y evaluación funcional para un programa autónomo de ejercicio para un envejecimiento saludable.",
+		url: "https://www.upm.es/observatorio/vi/index.jsp?pageac=proyectos/ficha_proyecto.jsp&idp=18956",
+		tech: "Artificial Intelligence, ubiquitous systems, eHealth, 5G, robotics",
 	},
 	{
-		from: "2023",
-		to: "2022",
-		title: "Family Safety Campaign",
-		description: "An initiative to promote safety awareness among families in Quahog.",
-		url: "https://safefamilyquahog.com",
-		tech: "Workshops, Educational Material, Public Events",
+		from: "2022",
+		to: "2023",
+		title: "LeADS",
+		description: "Leading European Advanced Digital Skills.",
+		url: "https://advancedskills.eu",
+		tech: "Advanced Digital Skills, Educational Material, Public Events",
 	},
 	{
-		from: "2020",
-		to: "2021",
-		title: "Road Safety Initiative",
-		description: "A campaign to enhance road safety and reduce accidents in Quahog.",
-		url: "https://roadsafetyquahog.com",
-		tech: "Traffic Analysis, Signage Improvement, Public Awareness",
+		from: "2021",
+		to: "2019",
+		title: "POSITIVE",
+		description: "Maintaining and imPrOving the intrinSIc capaciTy Involving primary care and caregiVErs.",
+		url: "https://eithealth.eu/product-service/positive/",
+		tech: "Artificial Intelligence, ubiquitous systems, eHealth, 5G, robotics",
+	},
+	{
+		from: "2015",
+		to: "2016",
+		title: "TONE Project",
+		description: "Phone system developed in-house that is gradually taking over the functions of the deprecated Alcatel PABX internal phones",
+		url: "https://home.cern/news/news/cern/modern-telephony-system-cern",
+		tech: "Cloud computing, VoIP and WebRTC, DevOps, scalable systems",
 	}
 ]
 
 // Education --> #education section
 export const education = [
 	{
-		from: "1995",
-		to: "1999",
-		institute: "Quahog High School",
-		course: "High School Diploma",
-		location: "Quahog, Rhode Island"
+		from: "2017",
+		to: "2022",
+		institute: "Universidad Politécnica de Madrid",
+		course: "PhD in Computer Science",
+		location: "ETSIInf-UPM"
 	},
 	{
-		from: "1999",
-		to: "2003",
-		institute: "Quahog Community College",
-		course: "Bachelor of Science in Safety Management",
-		location: "Quahog, Rhode Island"
+		from: "2015",
+		to: "2014",
+		institute: "Universidad Politécnica de Madrid",
+		course: "Master in Informatics Engineering",
+		location: "ETSIInf-UPM"
 	},
 	{
-		from: "2003",
-		to: "2005",
-		institute: "Safety University",
-		course: "Master of Science in Safety Engineering",
-		location: "Boston, Massachusetts"
+		from: "2014",
+		to: "2008",
+		institute: "Universidad Politécnica de Madrid",
+		course: "Bachelor Degree in Computer Engineering",
+		location: "ETSIInf-UPM"
 	}
 ]
 
@@ -78,15 +149,11 @@ export const education = [
 export const contact = [
 	{
 		source_name: "Email",
-		source: "peter.griffin@example.com",
-	},
-	{
-		source_name: "Twitter",
-		source: "https://twitter.com/PeterGriffin",
+		source: "j.barambones@upm.es",
 	},
 	{
 		source_name: "LinkedIn",
-		source: "https://www.linkedin.com/in/peter-griffin",
+		source: "https://www.linkedin.com/in/jmbaram/",
 	},
 ]
 
@@ -94,66 +161,42 @@ export const contact = [
 
 export const certificates = [
 	{
-		issued: "2005",
-		title: "Certified Safety Inspector",
-		org: "National Safety Association",
-		url: "https://certi.com/url",
-		description: "Certification in safety inspection and risk assessment.",
-	},
-	{
-		issued: "2006",
-		title: "Emergency Response Training",
-		org: "Quahog Safety Department",
-		url: "https://certi.com/url",
-		description: "Training in handling emergency situations and crisis management.",
-	},
-	{
-		issued: "2007",
-		title: "Safety Leadership Workshop",
-		org: "Safety Leaders International",
-		url: "https://certi.com/url",
-		description: "Leadership training in safety management and communication.",
+		issued: "2020",
+		title: "Data Structures and Algorithms Specialization",
+		org: "UC San Diego",
+		url: "https://coursera.org/verify/specialization/KZZU5CNHL8BQ",
+		description: "Certification in Data Structures, Advanced Algorithms and Complexity, Algorithms on Strings and Graphs.",
 	}
 ]
 
 // Blogs --> #blogs section
 export const blogs = [
-	{
-		date: "12-July, 2023",
-		url: "https://medium.com/",
-		title: "Adventures in Safety Inspection",
-		description: "Join me in my humorous escapades as a safety inspector in Quahog.",
-		publisher: "Quahog Times",
-	},
-	{
-		date: "25-June, 2023",
-		url: "https://medium.com/",
-		title: "Top 10 Safety Tips for Families",
-		description: "Learn the best safety practices for families to ensure a secure environment.",
-		publisher: "Safety Monthly",
-	}
+	// {
+	// 	date: "12-July, 2023",
+	// 	url: "https://medium.com/",
+	// 	title: "Adventures in Safety Inspection",
+	// 	description: "Join me in my humorous escapades as a safety inspector in Quahog.",
+	// 	publisher: "Quahog Times",
+	// },
+	// {
+	// 	date: "25-June, 2023",
+	// 	url: "https://medium.com/",
+	// 	title: "Top 10 Safety Tips for Families",
+	// 	description: "Learn the best safety practices for families to ensure a secure environment.",
+	// 	publisher: "Safety Monthly",
+	// }
 ]
 
 // Achievements --> #achievement section
 export const achievements = [
 	{
-		title: "Safety Hero Award",
-		year: "2020",
-		decription: "Recognized for outstanding contributions to improving safety standards in Quahog."
+		title: "PhD Outstanding Cum Laude award",
+		year: "2023",
+		decription: "Recognized for outstanding contribution and PhD international mention (Spain-Japan)."
 	},
 	{
-		title: "Safety Innovation Prize",
-		year: "2018",
-		decription: "Awarded for implementing innovative safety measures that yielded significant improvements."
-	},
-	{
-		title: "Community Safety Ambassador",
+		title: "DRONE Expo research award (Chiba, Japan)",
 		year: "2015",
-		decription: "Honored for promoting safety awareness and education in the Quahog community."
-	},
-	{
-		title: "Safety Campaign Excellence",
-		year: "2013",
-		decription: "Acknowledged for leading a successful safety campaign that reached thousands of residents."
+		decription: "Awarded for implementing innovative distributed algorithms for drone teams as a member of PrendingerLab at NII"
 	}
 ]
